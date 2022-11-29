ccdda2e1d3caed5a09abc1f13378acc28a33107b
Fatih Öztürk <fatihcamuz@gmail.com>
2022-11-29 22:38:25 +0300

    Rename package name

--- core/ui/src/main/java/com/reddity/app/ui/theme/Shape.kt
+++ core/ui/src/main/java/com/reddity/app/ui/theme/Shape.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.reddity.app.composeui.theme
+package com.reddity.app.ui.theme
 
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Shapes
--- core/ui/src/main/java/com/reddity/app/ui/theme/Type.kt
+++ core/ui/src/main/java/com/reddity/app/ui/theme/Type.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.reddity.app.composeui.theme
+package com.reddity.app.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
