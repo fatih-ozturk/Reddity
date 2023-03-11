87bb4f639bcf5d57a2cf671e99112db71c95231c
Fatih OZTURK <fatihcamuz@gmail.com>
2023-03-11 23:45:33 +0300

    Set limit for authentication retries

--- core/network/src/main/java/com/reddity/app/network/utils/ReddityAuthenticator.kt
+++ core/network/src/main/java/com/reddity/app/network/utils/ReddityAuthenticator.kt
@@ -30,23 +30,42 @@ class ReddityAuthenticator @Inject constructor(
     private val authManager: AuthManager
 ) : Authenticator {
 
-    override fun authenticate(route: Route?, response: Response): Request {
-        val originalRequest = response.request
-        val requestBuilder = originalRequest.newBuilder()
-
+    override fun authenticate(route: Route?, response: Response): Request? = synchronized(this) {
         runBlocking {
+            val currentRetry = responseCount(response)
+            if (currentRetry > RETRY_COUNT) {
+                authManager.clearAuthState()
+                return@runBlocking null
+            }
+
+            val originalRequest = response.request
+            val requestBuilder = originalRequest.newBuilder()
+
             reddityAuthManager.refreshAccessToken()
 
             val authState = authManager.getCurrentAuthState()
 
             Timber.e("Refresh access token %s", authState.accessToken)
+            Timber.e("Current retry %s", currentRetry)
 
             requestBuilder.header(
                 "Authorization",
                 "Bearer " + authState.accessToken
             )
+            return@runBlocking requestBuilder.build()
         }
+    }
 
-        return requestBuilder.build()
+    private fun responseCount(response: Response?): Int {
+        var result = 1
+        var tempResponse = response
+        while ((tempResponse?.priorResponse.also { tempResponse = it }) != null) {
+            result++
+        }
+        return result
+    }
+
+    companion object {
+        private const val RETRY_COUNT = 3
     }
 }
